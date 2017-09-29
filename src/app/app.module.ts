2daac1ebf4f0c19dd9b610070717d4252bcc3f9e
Santhosh <sandyorbit@gmail.com>
2017-09-29 15:43:56 -0700

    UI layout completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToggleComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
